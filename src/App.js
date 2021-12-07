fa190571dd07058075f231b72856c5059085ba13
Graham Hagenah <ghagenah@predictiveindex.com>
2021-12-06 21:34:51 -0500

    fixing autodeal and mobile optimizing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from "react";
 import CountUp from 'react-countup';
 import './App.css';
 
@@ -24,27 +24,67 @@ const Hand = ({ name, hand, total, gameover }) => {
   )
 }
 
-const Controls = ({ gameover, deal, stand, playerWins }) => {
+// const MiniHand = ({ name, hand, total, gameover }) => {
 
-  if(!gameover)
+//   return (
+//     <div class="mini-hand">
+//       <div class="vertical-divider"></div>
+//       <div class="horizontal-divider"></div>
+//       <ul>		
+//         <li class="name">{name}</li>
+//         {(total === 0) && <li class="total"></li>}
+//         {(total > 0) && <li class="total">{total}</li>}
+//         <li><div class="card-value">{hand[0]}</div></li>
+//         <li><div class="card-value">{hand[1]}</div></li>
+//         <li><div class="card-value">{hand[2]}</div></li>
+//         <li><div class="card-value">{hand[3]}</div></li>
+//         <li><div class="card-value">{hand[4]}</div></li>
+//         <li><div class="card-value">{hand[5]}</div></li>
+//       </ul>	
+//     </div>
+//   )
+// }
+
+const Controls = ({ gameover, deal, stand, playerWins, clear, playersTurn }) => {
+
+  if(!gameover && playersTurn)
     return (
       <div id="board-bottom">
         <div class="buttons">
-          <button onClick = { deal() } class="pushable">
+          <button onClick = { deal } class="pushable">
             <span class="front">Deal</span>
           </button>
           <button onClick = { stand } class="pushable">
             <span class="front">Stand</span>
           </button>
           <button onClick = { stand } class="pushable">
-            <span class="front">Double</span>
-          </button>
-          <button onClick = { stand } class="pushable">
-            <span class="front">Reset</span>
-          </button>
+           <span class="front">Double</span>
+         </button>
+          {/* <button onClick = { clear } class="pushable">
+            <span class="front">View</span>
+          </button> */}
         </div>
       </div>
     )
+  else if(!gameover && !playersTurn)
+  return (
+    <div id="board-bottom">
+      <div class="buttons">
+        <button class="not-pushable">
+          <span class="front">Deal</span>
+        </button>
+        <button class="not-pushable">
+          <span class="front">Stand</span>
+        </button>
+        <button class="not-pushable">
+          <span class="front">Double</span>
+        </button>
+        {/* <button class="not-pushable">
+          <span class="front">View</span>
+        </button> */}
+      </div>
+    </div>
+  )
   else 
   return (
     <div id="board-bottom-gameover">
@@ -67,26 +107,32 @@ const Score = ( {score, change} ) => {
 
 // not sure how to handle a state in which numbers are blinking for a set period of time
 
-// const Help = () => {
-//   return (
-//     <div id="help">
-//       <img id="mute" alt="mute" src="mute.png"></img>
-//       <h2>?</h2>
-//     </div>
+const Help = () => {
+  return (
+    <div id="help">
+      <img id="mute" alt="mute" src="mute.png"></img>
+      <h2>?</h2>
+    </div>
     
-//   )
-// }
+  )
+}
 
 const Board = ( {score, gameover, turn, playerHand, playerTotal, dealerHand, dealerTotal}) => {
 
     if(turn === true) {
       return (
-        <Hand name="Player" hand={playerHand} total={playerTotal} gameover={gameover} />
+        <div class="board">
+          {/* <MiniHand name="Dealer" hand={playerHand} total={playerTotal} gameover={gameover} /> */}
+          <Hand name="Player" hand={playerHand} total={playerTotal} gameover={gameover} />
+        </div>
       )
     }
     else 
       return (
-        <Hand name="Dealer" hand={dealerHand} total={dealerTotal} gameover={gameover} />
+        <div class="board">
+          {/* <MiniHand name="Player" hand={playerHand} total={playerTotal} gameover={gameover} /> */}
+          <Hand name="Dealer" hand={dealerHand} total={dealerTotal} gameover={gameover} />
+        </div>
       )
 }
 
@@ -104,6 +150,21 @@ const App = ({cards}) => {
   const [playerWins, setplayerWins] = useState(false)
   const [score, setScore] = useState(200)
   const [change, setChange] = useState(0)
+  const ref = useRef(nextDealerPosition);
+
+  const clearState = () => {
+    setPlayerHand(Array(6).fill(''))
+    setPlayerTotal(0)
+    setDealerHand(Array(6).fill(''))
+    setDealerTotal(0)
+    setDeck(cards)
+    setNextPlayerPosition(0)
+    setNextDealerPosition(0)
+    togglePlayersTurn(true)
+    toggleGameOver(false)
+    setplayerWins(false)
+    ref.current = 0
+  }
 
   const total = (hand) => { 
     let sum = 0
@@ -131,26 +192,35 @@ const App = ({cards}) => {
   }
 
   const declareWinner = (total) => {
-    if(total > 21) {
-      if(playersTurn) {
-        setplayerWins(false)
-        setChange(-20)
-        setScore(score+change)
-        //write code here to wait three seconds and then return to intial state with updated score
-      }
-      else {
-        setplayerWins(true)
-        setChange(20)
-        setScore(score+change)
-      }
+    if(total > 21 && playersTurn) {
+      setplayerWins(false)
+      setChange(-20)
+      setScore(score+change)
       toggleGameOver(true)
+      const intervalId = setInterval(function() {
+        clearState()
+        clearInterval(intervalId)
+      }, 3000)
+    }
+    else if(total > 21 && !playersTurn){
+      setplayerWins(true)
+      setChange(20)
+      setScore(score+change)
+      toggleGameOver(true)
+      const intervalId = setInterval(function() {
+        clearState()
+        clearInterval(intervalId)
+      }, 3000)
     }
   }
 
   const deal = () => { 
-    if(playersTurn === true)
-      return hitPlayer
-    else return hitDealer
+    if(playersTurn) {
+      return hitPlayer()
+    }
+    else {
+      return hitDealer()
+    }
   }
 
   const hitPlayer = () => {
@@ -174,29 +244,46 @@ const App = ({cards}) => {
 
   const hitDealer = () => {
     const newDeck = deck
-    const newNextDealerPosition = nextDealerPosition
+    const newNextDealerPosition = ref.current
     const newDealerHand = dealerHand
     newDealerHand[newNextDealerPosition] = newDeck.splice(getRandomInt(0, newDeck.length), 1)[0]
     setDealerHand(newDealerHand)
     setNextDealerPosition(newNextDealerPosition + 1)
+    ref.current = ref.current + 1;
     setDeck(newDeck)
     const newDealerTotal = total(dealerHand)
     setDealerTotal(newDealerTotal)
     declareWinner(newDealerTotal)
   }
 
-  const stand = () => { togglePlayersTurn(!playersTurn) }
+  //If you want to perform an action on state update, you need to use the useEffect hook, much like using componentDidUpdate in class components since the setter returned by useState doesn't have a callback pattern
 
-  const whoseTurn = () => { 
-    if(playersTurn === true)
-      return 'Player'
-    else return 'Dealer'
+  const stand = () => { 
+    let newPlayersTurn = !playersTurn
+    togglePlayersTurn(!playersTurn) 
+    if(!newPlayersTurn){
+      return autoDeal()
+    }
   }
 
+  const autoDeal = () => { 
+    const intervalId = setInterval(function() {
+      if (total(dealerHand) < 17)
+        hitDealer()
+      else {
+        // Reset state ready for next time.
+        togglePlayersTurn(true) 
+        clearInterval(intervalId)
+      }
+    }, 1000)
+  }
 
-return(
+return (
  		<div>	
-      <Score score={score} change={change} />
+     {/* <Help /> */}
+      <Score 
+        score={score} 
+        change={change} />
       <Board 
         score={score}
         gameover={gameOver}
@@ -204,9 +291,14 @@ return(
         playerHand={playerHand} 
         playerTotal={playerTotal} 
         dealerHand={dealerHand} 
-        dealerTotal={dealerTotal} 
-      />		
-      <Controls gameover = {gameOver} deal = {deal} stand = { stand } playerWins={playerWins} />
+        dealerTotal={dealerTotal} />		
+      <Controls 
+        gameover = {gameOver}
+        deal = {deal} 
+        stand = {stand} 
+        playerWins={playerWins}
+        clear={clearState}
+        playersTurn={playersTurn} />
 		</div>	
     )
 }
